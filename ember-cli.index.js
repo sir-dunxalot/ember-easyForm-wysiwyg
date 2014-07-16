36fb2051a51ed7426cb41ad010d5f12ece52026f
Duncan Walker <walkerdu@bc.edu>
2014-07-15 20:06:05 -0400

    Create ember-cli.index.js

---
+++ ember-cli.index.js
@@ -0,0 +1,38 @@
+export default {
+  name: 'easyform-wysiwyg',
+
+  initialize: function() {
+
+    Ember.EasyForm.WYSIWYG = Ember.EasyForm.TextArea.extend({
+    
+      renderEditor: function() {
+        var _this = this;
+    
+        _this.$().jqte({
+          color: false,
+          fsize: false,
+          format: false,
+          indent: false,
+          outdent: false,
+          placeholder: _this.get('placeholder'),
+          source: false, // Code
+          sub: false, // Subscript
+          strike: false,
+          sup: false, // Superscript
+    
+          change: function() {
+            Em.run.debounce(_this, _this.changed, 200);
+          }
+        });
+      }.on('didInsertElement'),
+    
+      changed: function() {
+        this.set('value', $('.jqte_editor').html());
+      },
+    
+    });
+    
+    Ember.EasyForm.Config.registerInputType('wysiwyg', Ember.EasyForm.WYSIWYG);
+
+  }
+};
